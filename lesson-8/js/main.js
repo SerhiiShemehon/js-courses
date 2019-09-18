154ce22d2ad32b63f4adbde3b8b402018767442f
SerhiiShemehon <sergei.shemegon.ua@gmail.com>
2019-09-19 00:08:09 +0300

    lesson 8

---
+++ lesson-8/js/main.js
@@ -0,0 +1,105 @@
+// Создайте базовый класс Figure, который будет хранить координаты(x, y) и цвет фигуры.На базе класса Figure создайте три класса– Line, Rect, Circle, каждый из которых создает соответствующую фигуру.Пример создания экземпляров каждого класса и параметры фигур:
+
+// var line = new Line(50, 250, 200, 200, 'red'); // x1, y1, x2, y2, color
+// var circle = new Circle(120, 120, 50, 'green'); // x, y, r, color
+// var rect = new Rect(260, 130, 60, 120, 'blue'); // x, y, w, h, color
+
+// Все три класса - наследника имеют метод draw для рисования фигуры с соответствующими параметрами(координаты, размеры, цвет).Фигуры рисуются на Canvas.Для рисования на канвасе создайте еще один класс– Canvas, в котором инициализируется элемент < canvas > из DOM.Класс Canvas– final, он не наследуется.В этом классе есть метод add, который и отображает созданные вами фигуры на странице.Обратите внимание, добавлять фигуры на канвас можно как по отдельности, так и списком.
+
+// var drawArea = new Canvas('canvasID');
+// drawArea.add(line);
+// drawArea.add(circle, rect);
+// <canvas id = "canvasID" > </canvas>
+
+// ET = 2h
+// AT = 40m
+
+
+// -----------------------------------------------------
+
+// Figure для хранения координат, цвета и метода для отрисовки (так как в нем есть код который подходит для всех данных фигур и чтобы его не повторять в каждой из них)
+class Figure {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+  }
+}
+
+// Rect для отрисовки квадрата
+class Rect extends Figure {
+  constructor(x, y, w, h, color) {
+    super(x, y, color);
+    this.w = w;
+    this.h = h;
+  }
+
+  draw(ctx) {
+    super.draw(ctx);
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+}
+
+// Circle для отрисовки круга
+class Circle extends Figure {
+  constructor(x, y, r, color) {
+    super(x, y, color);
+    this.r = r;
+  }
+
+  draw(ctx) {
+    super.draw(ctx);
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+// Line для отрисовки линии 
+class Line extends Figure {
+  constructor(x1, y1, x2, y2, color) {
+    super(x1, y1, color);
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  draw(ctx) {
+    super.draw(ctx);
+    ctx.strokeStyle = this.color;
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x2, this.y2);
+    ctx.stroke();
+  }
+}
+
+// для инициализации < canvas > из DOM.И имеет метод add, который и отображает созданные фигуры на странице
+class Canvas {
+  constructor(elem){
+    this.canvas = document.getElementById(elem);
+  }
+
+  add() {
+    if (this.canvas.getContext) {
+      let ctx = this.canvas.getContext('2d');
+
+      for(let i = 0; i < arguments.length; i++){
+        arguments[i].draw(ctx);
+      }
+    } else {
+      return alert('Ваш браузер не поддерживает Сanvas');
+    }
+  }
+}
+
+let line = new Line(50, 250, 200, 200, 'red'); // x1, y1, x2, y2, color
+let circle = new Circle(120, 120, 50, 'green'); // x, y, r, color
+let rect = new Rect(260, 130, 60, 120, 'blue'); // x, y, w, h, color
+let drawArea = new Canvas('canvasID');
+
+
+drawArea.add(line);
+drawArea.add(circle, rect);
